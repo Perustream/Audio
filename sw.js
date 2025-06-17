5ab6fd1fbb176eb3cc3cb55ddd5b6834c7444a43
Perustream <micasaperu21@gmail.com>
2025-06-16 23:57:44 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,12 @@
-self.addEventListener('install', (event) => {
+self.addEventListener('install', (e) => {
   console.log('Service Worker instalado');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  console.log('Service Worker activado');
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(fetch(e.request));
 });
