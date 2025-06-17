f285b53379f26b9e59e32630bdf37c823633c968
Perustream <micasaperu21@gmail.com>
2025-06-16 23:53:12 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,18 +1,3 @@
-
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open('radio-cache').then(function(cache) {
-      return cache.addAll([
-        'index_radio_pasion_pwa.html',
-        'manifest.json'
-      ]);
-    })
-  );
-});
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener('install', (event) => {
+  console.log('Service Worker instalado');
 });
